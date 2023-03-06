5b46e7dd697890d7db90bd20185f44b9b6a42eb1
Ste-Janu <122880008+Ste-Janu@users.noreply.github.com>
2023-03-06 11:31:25 -0300

    atividades

--- Atividades/ExerciciosJS/Ex3.js
+++ Atividades/ExerciciosJS/Ex3.js
@@ -1,7 +1,6 @@
- const gerarNum = () => {
-    let numero = Math.random()
-    let resultado = document.getElementById("resultado")
-    numero = Math.round(numero *100)
-    resultado.innerHTML = numero
-    
- }
+const gerarNum = () => {
+   let numero = Math.random()
+   let resultado = document.getElementById("resultado")
+   numero = Math.round(numero *100)
+   resultado.innerHTML = numero
+}
---
+++ Atividades/Sites/Financeiro/script.js
@@ -0,0 +1,23 @@
+const transacoesUl = document.querySelector('#transacoes');
+console.log(transacoesUl)
+
+// const dummytransacoes = [
+//     {id: 123, amount: 150, date: 06-03-2022},
+//     {id: 456, amount: 350, date: 06-03-2022},
+//     {id: 789, amount: -29, date: 06-03-2022},
+//     {id: 987, amount: -45, date: 06-03-2022}
+// ];
+
+// const addTransacoesNoDom = transacoes =>{
+//     const operador = transacoes.amount < 0 ? "-" : "+";
+//     const CSSclass = transacoes.amount < 0 ? "minus" : "plus";
+//     const amoutSemOp = Math.abs(transacoes.amount)
+//     const li = document.createElement("li")
+
+//     li.classList.add(CSSclass)
+//     li.innerHTML = (`${transacoes.id} <span> ${operador} R$ ${amoutSemOp} </span><button id="delete-btn">x</button>`);
+    
+
+// }
+
+// addTransacoesNoDom(dummytransacoes[2])
